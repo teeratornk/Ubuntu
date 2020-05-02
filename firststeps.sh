17a551517bedd31e3ede4276dfa370a6c7ac490f
Carlos A. S. Ferreira <casf.eq@gmail.com>
2020-05-02 14:29:30 -0300

    Corrected main script

--- firststeps.sh
+++ firststeps.sh
@@ -1,7 +1,7 @@
 export password="123456"
 export home="/home/desktop1"
-export petscVersion="3.13.0"
-export paraviewVersion="5.8"
+export petscVersion="3.13.0" # Find PETSc version on https://www.mcs.anl.gov/petsc/download/index.html
+export paraviewVersion="5.8" # Find Paraview version on https://www.paraview.org/download/ 
 export paraviewVersionName="5.8.0-MPI-Linux-Python3.7-64bit"
 export useremail="user@mail"
 export username="name"
@@ -27,10 +27,10 @@ export username="name"
 # Dropbox
 python3 ./py/dropbox.py ${password} ${home}
 
-# Find PETSc version on https://www.mcs.anl.gov/petsc/download/index.html
+# PETSc
 python3 ./py/petsc.py ${home} ${petscVersion}
 
-# Find Paraview version on https://www.paraview.org/download/ 
+# Paraview
 python3 ./py/paraview.py ${password} ${home} ${paraviewVersion} ${paraviewVersionName}
 
 # Mendeley
