aa57cc7ffca6c8b929d2370ede292b2e34cee944
Carlos A. S. Ferreira <casf.eq@gmail.com>
2020-05-03 14:34:19 -0300

    Added hist to git config

--- sh/git.sh
+++ sh/git.sh
@@ -1,3 +1,4 @@
 git config --global credential.helper store
 git config --global user.email "$1"
 git config --global user.name "$2"
+git config --global alias.hist 'log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
