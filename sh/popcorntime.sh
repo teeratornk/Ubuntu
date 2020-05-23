305161f0857e9f52deaa33b31285325f478d8306
casfeq <casf.eq@gmail.com>
2020-05-23 00:45:02 -0300

    Corrected some sudo routines

--- sh/popcorntime.sh
+++ sh/popcorntime.sh
@@ -1,9 +1,9 @@
 cd sh
-echo "$1" | sudo mkdir /opt/popcorntime
-echo "$1" | sudo cp -r popcorntime.desktop /usr/share/applications/
+echo "$1" | sudo -S mkdir /opt/popcorntime/
+echo "$1" | sudo -S cp -r popcorntime.desktop /usr/share/applications/
 cd ~/Downloads
 echo "$1" | sudo -S apt -y install libcanberra-gtk-module libgconf-2-4 unzip
-wget 'https://get.popcorntime.app/build/Popcorn-Time-0.4.3-linux64.zip'
-echo "$1" | sudo unzip Popcorn-Time-0.4.3-linux64.zip -d /opt/popcorntime
-echo "$1" | sudo ln -sf /opt/popcorntime/Popcorn-Time /usr/bin/Popcorn-Time
-echo "$1" | sudo wget -O /opt/popcorntime/popcorn.png https://upload.wikimedia.org/wikipedia/commons/d/df/Pctlogo.png
+wget 'https://dl.getpopcorntime.is/Popcorn-Time-linux64.tar.xz'
+echo "$1" | sudo -S tar -C /opt/popcorntime/ -xf Popcorn-Time-linux64.tar.xz
+echo "$1" | sudo -S ln -sf /opt/popcorntime/Popcorn-Time /usr/bin/Popcorn-Time
+echo "$1" | sudo -S wget -O /opt/popcorntime/popcorn.png https://upload.wikimedia.org/wikipedia/commons/d/df/Pctlogo.png
--- sh/utilities.sh
+++ sh/utilities.sh
@@ -1,6 +1,6 @@
 cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 echo "$1" | sudo -S dpkg -i google-chrome-stable_current_amd64.deb
-echo "$1" | sudo apt -y remove --purge firefox
+echo "$1" | sudo -S apt -y remove --purge firefox
 echo "$1" | sudo -S apt -y install gimp pinta mpv
 echo "$1" | sudo -S snap install wps-office spotify ao
